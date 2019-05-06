6b9a7edde3449730384e6387fca2cf8af9d09b29
claudito <1107000044@upci.edu.pe>
2019-05-05 21:12:45 -0500

    edit config

--- config/database.php
+++ config/database.php
@@ -2,13 +2,13 @@
 
 set_time_limit(5000);
 define("URL","http://localhost/contacto/");
-//define("URL","http://200.60.144.12/contacto/");
+//define("URL","https://contactounet.com/");
 date_default_timezone_set('America/Lima');
 define("KEY","adecb2e24eb1c1f24edd097e7aba7967-contacto/");
 define("FOLDER","/contacto/");
 //define("SERVER", "localhost");
-define("SERVER", "perutec.com.pe");
-define("USER", "contacto");
+define("SERVER", "localhost");
+define("USER", "root");
 define("PASS", "csglobal2019");
 //define("PASS", "");
 define("BD", "db_controlnet");
--- test.php
+++ test.php
@@ -1,3 +1,31 @@
+<?php 
 
+//include'vendor/autoload.php';
+
+
+try {
+
+include'vendor/jossmp/sunatphp/src/autoload.php';
+$company = new \Sunat\Sunat( true, true );
+
+	$numero = "10467942820";
+	
+	$ruc = ( isset($numero))? $numero : false;
+	$search1 = $company->search( $ruc );
+	
+	echo $search1->json();
+
+	
+} catch (Exception $e) {
+
+echo "Error: ".$e->getMessage();
+
+	
+ }
+
+
+
+
+ ?>
 
 

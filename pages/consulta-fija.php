970769523d908dc07281840f0df289ddcfcf77cc
claudito <1107000044@upci.edu.pe>
2019-05-09 18:27:12 -0500

    ultimos cambios

---
+++ pages/consulta-fija.php
@@ -0,0 +1,137 @@
+<form >
+	
+
+<div class="form-group row">
+<label class="col-sm-2 col-form-label">Num. Telefóno</label>
+<div class="col-sm-4">
+<input type="text" name="" class="form-control">
+</div>
+<label class="col-sm-2 col-form-label">Petición</label>
+<div class="col-sm-4">
+<input type="text" name="" class="form-control">
+</div>
+</div>
+
+<div class="form-group row">
+<label class="col-sm-2 col-form-label">Operación</label>
+<div class="col-sm-4">
+<input type="text" name="" class="form-control">
+</div>
+<label class="col-sm-2 col-form-label">DNI</label>
+<div class="col-sm-4">
+<input type="text" name="" class="form-control">
+</div>
+</div>
+
+
+<div class="form-group row">
+<label class="col-sm-2 col-form-label">SubEstados</label>
+<div class="col-sm-4">
+<select name="" id="" class="form-control">
+<option value="">(Todos)</option>
+</select>
+</div>
+<label class="col-sm-2 col-form-label">Clasificación</label>
+<div class="col-sm-4">
+<select name="" id="" class="form-control">
+<option value="">(Todos)</option>
+</select>
+</div>
+</div>
+
+<div class="form-group row">
+<label class="col-sm-2 col-form-label">F. Creacion</label>
+<div class="col-sm-2">
+<input type="date" name="" id="" class="form-control" value="<?=  date('Y-m-d') ?>">
+</div>
+<div class="col-sm-2">
+<input type="date" name="" id="" class="form-control" value="<?=  date('Y-m-d') ?>">
+</div>
+
+</div>
+
+<div class="form-group row">
+<label class="col-sm-2 col-form-label">F.Alta</label>
+<div class="col-sm-2">
+<input type="text" name="" id="" class="form-control" value="">
+</div>
+<div class="col-sm-2">
+<input type="text" name="" id="" class="form-control" value="">
+</div>
+
+</div>
+
+
+<div class="form-group row">
+<label class="col-sm-2 col-form-label">F. Reg. Movistar</label>
+<div class="col-sm-2">
+<input type="text" name="" id="" class="form-control" value="">
+</div>
+<div class="col-sm-2">
+<input type="text" name="" id="" class="form-control" value="">
+</div>
+
+</div>
+
+
+<div class="form-group row">
+<label class="col-sm-2 col-form-label">Usuarios</label>
+<div class="col-sm-4">
+<select name="" id="" class="form-control">
+<option value="">(Todos)</option>
+</select>
+</div>
+
+</div>
+
+
+<div class="form-group row">
+<label class="col-sm-2 col-form-label">Punto Venta </label>
+<div class="col-sm-4">
+<select name="" id="" class="form-control">
+<option value="">(Todos)</option>
+</select>
+</div>
+<label class="col-sm-2 col-form-label">Cod. Temis </label>
+<div class="col-sm-2">
+<input type="text" name="" id="" class="form-control">
+</div>
+</div>
+
+<div class="form-group row">
+<label class="col-sm-2 col-form-label">Tipo Operación </label>
+<div class="col-sm-4">
+<select name="" id="" class="form-control">
+<option value="">(Todos)</option>
+</select>
+</div>
+
+</div>
+
+<div class="form-group row">
+<label class="col-sm-2 col-form-label">Tipo Producto </label>
+<div class="col-sm-4">
+<select name="" id="" class="form-control">
+<option value="">(Todos)</option>
+</select>
+</div>
+
+</div>
+
+
+<div class="form-group row">
+<label class="col-sm-2 col-form-label"> </label>
+<div class="col-sm-4">
+
+<button class="btn btn-primary">Consultar</button> <button class="btn btn-success">Exportar</button>
+
+
+</div>
+
+</div>
+
+
+
+</form>
+<hr>
+<p>Transacciones Operaciones Fija <br> Numero de Fijas</p> <br> <p><label><input type="checkbox" name="" id="" checked>Paginar</label></p>
